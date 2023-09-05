1d45c5f2d10e88c8bf131085f5345c49a91094dd
Piotr Tutak <piotr.tutak@getindata.com>
2023-09-05 11:38:39 +0200

    commmand cli in separate method

--- src/dbt_databricks_factory/cli.py
+++ src/dbt_databricks_factory/cli.py
@@ -67,6 +67,66 @@ def create_job(
     pretty: bool,
 ) -> None:
     """Create a job."""  # noqa: DCO020, DCO050
+    create_job_cli(
+        job_name,
+        manifest_file,
+        project_dir,
+        profiles_dir,
+        cron_schedule,
+        job_cluster,
+        task_cluster,
+        default_task_cluster,
+        library,
+        git_url,
+        git_branch,
+        git_commit,
+        git_tag,
+        git_provider,
+        pretty,
+    )
+
+
+def create_job_cli(
+    job_name: str,
+    manifest_file: str,
+    project_dir: str,
+    profiles_dir: str,
+    cron_schedule: str | None,
+    job_cluster: list[tuple[str, str]],
+    task_cluster: list[tuple[str, str]],
+    default_task_cluster: str | None,
+    library: list[str],
+    git_url: str,
+    git_branch: str | None,
+    git_commit: str | None,
+    git_tag: str | None,
+    git_provider: str,
+    pretty: bool,
+    output_file: str | None = None,
+) -> None:
+    """Create a job.
+
+    Args:
+        job_name (str): Name of the job to create.
+        manifest_file (str): Path to dbt manifest file.
+        project_dir (str): Path to dbt project directory.
+        profiles_dir (str): Path to dbt profiles directory.
+        cron_schedule (str | None): Cron schedule for the job.
+        job_cluster (list[tuple[str, str]]): Job cluster list. List contains of tuples of cluster names and config.
+        task_cluster (list[tuple[str, str]]): Task cluster list. List contains of tuples of cluster names and config.
+        default_task_cluster (str | None): Default task cluster name.
+        library (list[str]): List of libraries.
+        git_url (str): Git url.
+        git_branch (str | None): Git branch.
+        git_commit (str | None): Git commit.
+        git_tag (str | None): Git tag.
+        git_provider (str): Git provider.
+        pretty (bool): Pretty print the output.
+        output_file (str | None): Output file path.
+
+    Raises:
+        BadParameter: Either task cluster or default task cluster must be provided
+    """
     if len(task_cluster) == 0 and default_task_cluster is None:
         raise click.BadParameter("Either task cluster or default task cluster must be provided")
 
@@ -104,7 +164,12 @@ def create_job(
         ),
         schedule_config=ScheduleConfig(cron_schedule, "UTC") if cron_schedule is not None else None,
     )
-    click.echo(json.dumps(builder.build(manifest_file), indent=2 if pretty else None))
+    output = json.dumps(builder.build(manifest_file), indent=2 if pretty else None)
+    if output_file is not None:
+        with open(output_file, "w") as file:
+            file.write(output)
+    else:
+        click.echo(output)
 
 
 if __name__ == "__main__":
